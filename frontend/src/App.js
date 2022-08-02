6b2f1ad6aff26eb6eb918c8def5d19335a3e2657
unknown <priyongshupaul@gmail.com>
2022-08-02 20:44:47 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App(){
+	return(
+		<div>
+		  <header>
+		  <a href="/">MERN PAIN</a>
+		  </header>
+		<main>
+		  List products
+		</main>
+	    </div>
+	);
+}
+
+export default App;
